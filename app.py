95f10e9adb4110267f708fc374a318d51965fbf7
Hugh Keenan <hugh.keenan114@gmail.com>
2025-01-08 20:57:07 +0000

    create mildew_detector.py

--- app.py
+++ app.py
@@ -4,12 +4,14 @@ from app_pages.multipage import MultiPage
 # load pages scripts
 from app_pages.project_summary import project_summary_body
 from app_pages.image_visualizer import image_visualizer_body
+from app_pages.mildew_detector import mildew_detector_body
 
 app = MultiPage(app_name="Cherry Picker")  # Create an instance of the app
 
 # Add your app pages here using .add_page()
 app.add_page("Project Summary", project_summary_body)
 app.add_page("Image Visualizer", image_visualizer_body)
+app.add_page("Mildew Detector", mildew_detector_body)
 
 
 app.run()  # Run the app
