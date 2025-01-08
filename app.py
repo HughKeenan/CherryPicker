836eb7f520adaee2aff576fdcd491973f42c37c3
Hugh Keenan <hugh.keenan114@gmail.com>
2025-01-08 20:09:56 +0000

    create image_visualizer page

--- app.py
+++ app.py
@@ -3,11 +3,13 @@ from app_pages.multipage import MultiPage
 
 # load pages scripts
 from app_pages.project_summary import project_summary_body
+from app_pages.image_visualizer import image_visualizer_body
 
 app = MultiPage(app_name="Cherry Picker")  # Create an instance of the app
 
 # Add your app pages here using .add_page()
 app.add_page("Project Summary", project_summary_body)
+app.add_page("Image Visualizer", image_visualizer_body)
 
 
 app.run()  # Run the app
