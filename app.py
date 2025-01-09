77a8ca50e3203284abd89d85a2bfc334dc7defdd
Hugh Keenan <hugh.keenan114@gmail.com>
2025-01-09 20:46:00 +0000

    create ml_performance_metrics.py

--- app.py
+++ app.py
@@ -6,6 +6,7 @@ from app_pages.project_summary import project_summary_body
 from app_pages.image_visualizer import image_visualizer_body
 from app_pages.mildew_detector import mildew_detector_body
 from app_pages.project_hypothesis import project_hypothesis_body
+from app_pages.ml_performance_metrics import ml_performance_metrics
 
 app = MultiPage(app_name="Cherry Picker")  # Create an instance of the app
 
@@ -14,5 +15,6 @@ app.add_page("Project Summary", project_summary_body)
 app.add_page("Image Visualizer", image_visualizer_body)
 app.add_page("Mildew Detector", mildew_detector_body)
 app.add_page("Project Hypothesis", project_hypothesis_body)
+app.add_page("ML Performance Metrics", ml_performance_metrics)
 
 app.run()  # Run the app
